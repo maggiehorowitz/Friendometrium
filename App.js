5fede4b8d748393774cc354838da9ffffabed019
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-25 14:52:29 -0700

    login screen UI

---
+++ App.js
@@ -0,0 +1,189 @@
+// //Default HelloWorld Stage
+//
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello ReactNative Demo!</Text>
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// //Navigation Tutorial
+import * as React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Button, View, Text ,StyleSheet, TextInput, TouchableOpacity,Image, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+
+class LoginScreen extends React.Component {
+    state={
+      email:"",
+      password:""
+    }
+    render(){
+      return (
+        <View style={styles.container}>
+          <Text style={styles.logo}>Friendometrium</Text>
+          <View style={styles.inputView} >
+            <TextInput
+              style={styles.inputText}
+              placeholder="Email..."
+              placeholderTextColor="#003f5c"
+              onChangeText={text => this.setState({email:text})}/>
+          </View>
+          <View style={styles.inputView} >
+            <TextInput
+              secureTextEntry
+              style={styles.inputText}
+              placeholder="Password..."
+              placeholderTextColor="#003f5c"
+              onChangeText={text => this.setState({password:text})}/>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginBtn} onPress={() => this.props.navigation.navigate('Home')}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.loginText}>Signup</Text>
+          </TouchableOpacity>
+
+
+        </View>
+
+
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#003f5c',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{
+      fontWeight:"bold",
+      fontSize:46,
+      color:"#fb5b5a",
+      marginBottom:40
+    },
+    inputView:{
+      width:"80%",
+      backgroundColor:"#B0C4DE",
+      borderRadius:25,
+      height:50,
+      marginBottom:20,
+      justifyContent:"center",
+      padding:20
+    },
+    inputText:{
+      height:50,
+      color:"black"
+    },
+    forgot:{
+      color:"white",
+      fontSize:11
+    },
+    loginBtn:{
+      width:"80%",
+      backgroundColor:"#fb5b5a",
+      borderRadius:25,
+      height:50,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:40,
+      marginBottom:10
+    },
+    loginText:{
+      color:"white"
+    }
+  });
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Login Screen</Text>
+//         <Button
+//           title="Go to Details"
+//           onPress={() => this.props.navigation.navigate('Details')}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Login: LoginScreen,
+  Home: HomeScreen,
+});
+
+export default createAppContainer(RootStack);
+
+
+//MAPS
+
+//
+// import React from 'react';
+// import MapView from 'react-native-maps';
+// import { StyleSheet, Text, View, Dimensions } from 'react-native';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <MapView style={styles.mapStyle} />
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   mapStyle: {
+//     width: Dimensions.get('window').width,
+//     height: Dimensions.get('window').height,
+//   },
+// });
