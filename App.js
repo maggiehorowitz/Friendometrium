df254c76c14b0091b0f936c7058303487783cf2f
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-25 14:55:24 -0700

    navigation drawer implementation
    go between home,profile,map,forum pages

--- App.js
+++ App.js
@@ -1,189 +1,202 @@
-// //Default HelloWorld Stage
-//
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-//
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello ReactNative Demo!</Text>
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// //Navigation Tutorial
-import * as React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
+import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { Button, View, Text ,StyleSheet, TextInput, TouchableOpacity,Image, FlatList } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack"
 import { Ionicons } from '@expo/vector-icons';
 
 
-class LoginScreen extends React.Component {
-    state={
-      email:"",
-      password:""
-    }
-    render(){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.logo}>Friendometrium</Text>
-          <View style={styles.inputView} >
-            <TextInput
-              style={styles.inputText}
-              placeholder="Email..."
-              placeholderTextColor="#003f5c"
-              onChangeText={text => this.setState({email:text})}/>
-          </View>
-          <View style={styles.inputView} >
-            <TextInput
-              secureTextEntry
-              style={styles.inputText}
-              placeholder="Password..."
-              placeholderTextColor="#003f5c"
-              onChangeText={text => this.setState({password:text})}/>
-          </View>
-          <TouchableOpacity>
-            <Text style={styles.forgot}>Forgot Password?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.loginBtn} onPress={() => this.props.navigation.navigate('Home')}>
-            <Text style={styles.loginText}>LOGIN</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={styles.loginText}>Signup</Text>
-          </TouchableOpacity>
+//TODO: Separate this out into different classes, better architecture and cleaning
 
+const Header =({name, openDrawer})=> (
+  <View style={styles.header}>
+    <TouchableOpacity onPress={()=>openDrawer()}>
+      <Ionicons name="ios-menu" size={32} />
+    </TouchableOpacity>
+    <Text>{name}</Text>
+    <Text style={{width:50}}></Text>
+  </View>
+)
+const Home = ({navigation}) => (
+  <View style={styles.container}>
+    <Header name="Home" openDrawer={navigation.openDrawer}/>
+    <Image source ={require("./assets/friendometrium.jpg")} style={{width:"80%", height:"30%"}} resizeMode="contain"/>
+    <Text style={{padding:20}}>
+    Welcome to Friendometrium!
+    </Text>
+    <Text style={{padding:20}}>
+    Friendometrium is a great app for women to form a strong community for supporting women's health. Don't forget to check
+    out the maps section and the chat section!
+    </Text>
 
-        </View>
+  </View>
+)
 
+const Profile = ({navigation}) => (
+  <View style={styles.container}>
+    <Header name="Profile" openDrawer={navigation.openDrawer}/>
+    <Image source ={require("./assets/t_roberts.jpg")} style={{width:"80%", height:"30%"}} resizeMode="contain"/>
+    <Text style={{padding:20}}>
+    This is the Profile Page
+    </Text>
+    <Text style={{padding:20}}>
+    Add some information about the Friendometrium user here
+    </Text>
+  </View>
+)
 
-      );
-    }
+const Map = ({navigation}) => (
+  <View style={styles.container}>
+    <Header name="Map" openDrawer={navigation.openDrawer}/>
+    <Image source ={require("./assets/map.jpg")} style={{width:"80%", height:"30%"}} resizeMode="contain"/>
+    <Text style={{padding:20}}>
+    This is the Map page.
+    </Text>
+    <Text style={{padding:20}}>
+    Map 2nd block of text.
+    </Text>
+  </View>
+)
+
+const Forum = ({navigation}) => (
+  <View style={styles.container}>
+    <Header name="Forum" openDrawer={navigation.openDrawer}/>
+    <Image source ={require("./assets/chat.jpg")} style={{width:"80%", height:"30%"}} resizeMode="contain"/>
+    <Text style={{padding:20}}>
+    This is the Forum Chat page. It is not yet implemented.
+    </Text>
+    <Text style={{padding:20}}>
+    Option to add more text here.
+    </Text>
+  </View>
+)
+
+function Item({ item, navigate }) {
+  return (
+    <TouchableOpacity style={styles.listItem} onPress={()=>navigate(item.name)}>
+      <Ionicons name={item.icon} size={32} />
+      <Text style={styles.title}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+}
+
+class Sidebar extends React.Component {
+  state = {
+      routes:[
+          {
+              name:"Home",
+              icon:"ios-home"
+          },
+          {
+              name:"Profile",
+              icon:"ios-contact"
+          },
+          {
+              name:"Map",
+              icon:"ios-map"
+          },
+          {
+              name:"Forum",
+              icon:"ios-chatboxes"
+          },
+      ]
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#003f5c',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logo:{
-      fontWeight:"bold",
-      fontSize:46,
-      color:"#fb5b5a",
-      marginBottom:40
-    },
-    inputView:{
-      width:"80%",
-      backgroundColor:"#B0C4DE",
-      borderRadius:25,
-      height:50,
-      marginBottom:20,
-      justifyContent:"center",
-      padding:20
-    },
-    inputText:{
-      height:50,
-      color:"black"
-    },
-    forgot:{
-      color:"white",
-      fontSize:11
-    },
-    loginBtn:{
-      width:"80%",
-      backgroundColor:"#fb5b5a",
-      borderRadius:25,
-      height:50,
-      alignItems:"center",
-      justifyContent:"center",
-      marginTop:40,
-      marginBottom:10
-    },
-    loginText:{
-      color:"white"
-    }
-  });
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Login Screen</Text>
-//         <Button
-//           title="Go to Details"
-//           onPress={() => this.props.navigation.navigate('Details')}
-//         />
-//       </View>
-//     );
-//   }
-// }
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
+  render(){
+      return (
+          <View style={styles.container}>
+              <Image source={require("./assets/t_roberts.jpg")} style={styles.profileImg}/>
+              <Text style={{fontWeight:"bold",fontSize:16,marginTop:10}}>Tomi-Ann Roberts</Text>
+              <Text style={{color:"gray",marginBottom:10}}>troberts@coloradocollege.edu</Text>
+              <View style={styles.sidebarDivider}></View>
+              <FlatList
+                  style={{width:"100%",marginLeft:30}}
+                  data={this.state.routes}
+                  renderItem={({ item }) => <Item  item={item} navigate={this.props.navigation.navigate}/>}
+                  keyExtractor={item => item.name}
+              />
+          </View>
+      )
   }
 }
 
-const RootStack = createStackNavigator({
-  Login: LoginScreen,
-  Home: HomeScreen,
+const Drawer = createDrawerNavigator(
+  {
+    Home:{ screen: Home},
+    Profile:{ screen: Profile},
+    Map:{ screen: Map},
+    Forum:{ screen: Forum}
+
+  },
+  {
+    initialRouteName: "Home",
+    unmountInactiveRoutes: true,
+    headerMode: "none",
+    contentComponent: props => <Sidebar {...props} />
+  }
+)
+
+const AppNavigator = createStackNavigator(
+  {
+    Drawer : {screen: Drawer},
+  },
+  {
+    initialRouteName: "Drawer",
+    headerMode: "none",
+    unmountInactiveRoutes: true
+  }
+)
+
+const AppContainer = createAppContainer(AppNavigator);
+
+
+
+export default class App extends React.Component {
+  render(){
+
+    return (
+      <AppContainer />
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    paddingTop:40,
+    alignItems:"center",
+    flex:1
+
+  },
+  listItem:{
+      height:60,
+      alignItems:"center",
+      flexDirection:"row",
+  },
+  title:{
+      fontSize:18,
+      marginLeft:20
+  },
+  header:{
+    width:"100%",
+    height:60,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    paddingHorizontal:20
+  },
+  profileImg:{
+    width:80,
+    height:80,
+    borderRadius:40,
+    marginTop:20
+  },
+  sidebarDivider:{
+    height:1,
+    width:"100%",
+    backgroundColor:"lightgray",
+    marginVertical:10
+  }
 });
-
-export default createAppContainer(RootStack);
-
-
-//MAPS
-
-//
-// import React from 'react';
-// import MapView from 'react-native-maps';
-// import { StyleSheet, Text, View, Dimensions } from 'react-native';
-//
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <MapView style={styles.mapStyle} />
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   mapStyle: {
-//     width: Dimensions.get('window').width,
-//     height: Dimensions.get('window').height,
-//   },
-// });
