743cc6dad95b40fbd3f8f20714d5b305748200c5
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-27 13:36:28 -0700

    register-page
    added register page implementation

--- components/RegisterPage.js
+++ components/RegisterPage.js
@@ -1,43 +1,55 @@
-
-
-
-
-
-import React, {Component} from 'react';
-import { Button, View, Text , StyleSheet, TextInput, TouchableOpacity,Image, FlatList } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-
-
-
-export default class HomePage extends Component{
-  render(){
-    return(
-      <View style ={styles.container}>
-        <Text style = {styles.text}> Welcome to Registration Page </Text>
-        <Text style = {styles.text}> Fixing a bug, under construction </Text>
+// SignUp.js
+import React from 'react'
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+export default class SignUp extends React.Component {
+  state = { email: '', password: '', errorMessage: null }
+handleSignUp = () => {
+  // TODO: Firebase stuff...
+  console.log('handleSignUp')
+}
+render() {
+    return (
+      <View style={styles.container}>
+        <Text>Sign Up</Text>
+        {this.state.errorMessage &&
+          <Text style={{ color: 'red' }}>
+            {this.state.errorMessage}
+          </Text>}
+        <TextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <Button title="Sign Up" onPress={this.handleSignUp} />
         <Button
-          title = "Go back to login"
-          color = '#1abc9c'
-          onPress = {() =>this.props.navigation.navigate('Login')}
-          />
+          title="Already have an account? Login"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
       </View>
-
-    );
+    )
   }
 }
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#003f5c',
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  text: {
-    textAlign: 'center',
-    color:"black"
+  textInput: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 8
   }
-});
+})
