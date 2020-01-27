c6777db057af37066d06cbfaee71667fe7187db1
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-27 10:44:31 -0700

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -8,5 +8,29 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Get a reference to the database service
-var database = firebase.database();
+// // Get a reference to the database service
+// var database = firebase.database();
+//
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js"></script>
+//
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.7.0/firebase-analytics.js"></script>
+//
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAm-zCVjxHkhkuPVsdOPgB6I4aRd3xv3fk",
+//     authDomain: "friendometrium-app.firebaseapp.com",
+//     databaseURL: "https://friendometrium-app.firebaseio.com",
+//     projectId: "friendometrium-app",
+//     storageBucket: "friendometrium-app.appspot.com",
+//     messagingSenderId: "762531570751",
+//     appId: "1:762531570751:web:a5bd525a19de3e581b048a",
+//     measurementId: "G-SNDYDJBWE7"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
