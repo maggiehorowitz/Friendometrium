bbc738c2335954f684f16beb4a816bdf41b663be
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-28 13:49:56 -0700

    RegisterBugFix
    Reverted back to old version after bug on RegisterPage Branch

--- components/RegisterPage.js
+++ components/RegisterPage.js
@@ -1,18 +1,18 @@
-
-import React, {Component} from 'react';
-import { Button, View, Text , StyleSheet, TextInput, TouchableOpacity,Image, FlatList } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-
-//Fix sign up bug
-
-export default class HomePage extends Component{
-  render(){
-    return(
-      <View style ={styles.container}>
-        <Text style = {styles.text}> Welcome to Registration Page </Text>
-        <Text style = {styles.text}> Fixing a bug, under construction </Text>
+// 
+// import React, {Component} from 'react';
+// import { Button, View, Text , StyleSheet, TextInput, TouchableOpacity,Image, FlatList } from 'react-native';
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+//
+// //Fix sign up bug
+//
+// export default class HomePage extends Component{
+//   render(){
+//     return(
+//       <View style ={styles.container}>
+//         <Text style = {styles.text}> Welcome to Registration Page </Text>
+//         <Text style = {styles.text}> Fixing a bug, under construction </Text>
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
 export default class SignUp extends React.Component {
