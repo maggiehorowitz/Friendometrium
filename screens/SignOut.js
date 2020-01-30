5c18db2fba03a526b52a914a3f8905f0e69a5312
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-01-29 21:41:25 -0700

    Added SignOut Button
    Allows the user to signout from Firebase

---
+++ screens/SignOut.js
@@ -0,0 +1,48 @@
+
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Share } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from "react-navigation-stack";
+import { Button } from 'react-native-elements'
+import { withFirebaseHOC } from '../config/Firebase'
+import { Ionicons } from '@expo/vector-icons';
+import MapPage from './MapPage';
+
+
+class SignOut extends Component {
+  handleSignout = async () => {
+    try {
+      await this.props.firebase.signOut()
+      this.props.navigation.navigate('Auth')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Not this user?  </Text>
+        <Button
+          title='Signout Now'
+          onPress={this.handleSignout}
+          titleStyle={{
+            color: '#F57C00'
+          }}
+          type='clear'
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default withFirebaseHOC(SignOut)
