1fec92320a7bc77d0106a88bd5168558cfa15915
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-02-07 10:19:52 -0700

    updates to modules

--- node_modules/react-native-maps/lib/ios/AirMaps/AIRMapMarkerManager.m
+++ node_modules/react-native-maps/lib/ios/AirMaps/AIRMapMarkerManager.m
@@ -27,13 +27,10 @@ RCT_EXPORT_MODULE()
     AIRMapMarker *marker = [AIRMapMarker new];
     [marker addTapGestureRecognizer];
     marker.bridge = self.bridge;
-    marker.isAccessibilityElement = YES;
-    marker.accessibilityElementsHidden = NO;
     return marker;
 }
 
 RCT_EXPORT_VIEW_PROPERTY(identifier, NSString)
-RCT_REMAP_VIEW_PROPERTY(testID, accessibilityIdentifier, NSString)
 //RCT_EXPORT_VIEW_PROPERTY(reuseIdentifier, NSString)
 RCT_EXPORT_VIEW_PROPERTY(title, NSString)
 RCT_REMAP_VIEW_PROPERTY(description, subtitle, NSString)
