e173e27e3166684e57e6826df5b2f763e3ed49fa
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-02-11 17:47:59 -0700

    w
    warn -i yellow

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import AppContainer from './navigation'
 import Firebase, { FirebaseProvider } from './config/Firebase'
 import store from './src/store'
 import { Provider } from 'react-redux'
+console.disableYellowBox = true;
 
 
 export default function App() {
